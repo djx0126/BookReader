863e2261474ab97b333d2660ece8ae5dabf66caa
Dave Dai <djx0126@126.com>
2012-04-15 18:03:48 +0800

    set font size by menu

--- src/com/bookreader/BookReaderActivity.java
+++ src/com/bookreader/BookReaderActivity.java
@@ -1,59 +1,94 @@
 package com.bookreader;
 
-import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.android.opengl.BaseGLSurfaceView;
 import com.android.opengl.BaseOpenGLActivity;
 import com.bookreader.config.Settings;
-import com.bookreader.file.FileHelper;
 import com.djx.bookreader.R;
 
 public class BookReaderActivity extends BaseOpenGLActivity {
+    private static final int MENU_ADD_FAVOR = 1;
+    private static final int MENU_FAVOR_LIST = 2;
+    private static final int MENU_SETTINGS = 3;
+    private static final int MENU_FONT_UP = 4;
+    private static final int MENU_FONT_DOWN = 5;
+
     TextView text1 = null;
 
     @Override
     public void initView() {
 
-        setContentView(R.layout.main);
-
-        myGLView = (BaseGLSurfaceView) this.findViewById(R.id.myView1);
-        myGLView.createDefaultRenderer();
-
-        // myGLView = new MainView(this);
-        // myGLView.createDefaultRenderer();
-        // setContentView(myGLView);
+        setMainView();
 
     }
 
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
         if (event.getAction() == KeyEvent.ACTION_UP) {
-            switch (keyCode) {
-            case KeyEvent.KEYCODE_MENU:
-                loadText();
-                return true;
-            default:
-                break;
-            }
+
         }
-        Log.d("[BookReaderActivity]", "other key event");
+        // Log.d("[BookReaderActivity]", "other key event");
         return super.onKeyUp(keyCode, event);
     }
 
-    private void loadText() {
-        text1 = (TextView) this.findViewById(R.id.textView1);
-        char[] buffer = new char[Settings.BUFFERSIZE];
-        int offset = Settings.OFFSET;
-        int charsRead = FileHelper.readFile(this, Settings.FILENAME, buffer, offset);
-        String s = "11";
-        if (charsRead > 0) {
-            s = FileHelper.unicodesToStr(buffer, charsRead);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        menu.add(0, MENU_ADD_FAVOR, MENU_ADD_FAVOR, R.string.MENU_ADD_FAVOR);
+        menu.add(0, MENU_FAVOR_LIST, MENU_FAVOR_LIST, R.string.MENU_FAVOR_LIST);
+        // menu.add(0, MENU_SETTINGS, MENU_SETTINGS, R.string.MENU_SETTINGS);
+        if (Settings.FONTSIZE <= Settings.FONTSIZEMAX) {
+            menu.add(0, MENU_FONT_UP, MENU_FONT_UP, R.string.MENU_FONT_UP);
+        }
+        if (Settings.FONTSIZE >= Settings.FONTSIZEMIN) {
+            menu.add(0, MENU_FONT_DOWN, MENU_FONT_DOWN, R.string.MENU_FONT_DOWN);
         }
 
-        Log.d("[BookReaderActivity]", "loadText:" + s);
-        text1.setText(s);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case MENU_ADD_FAVOR:
+            break;
+        case MENU_FAVOR_LIST:
+            break;
+        case MENU_SETTINGS:
+            // setSetView();
+            break;
+        case MENU_FONT_UP:
+            if (Settings.FONTSIZE <= Settings.FONTSIZEMAX) {
+                Settings.FONTSIZE += Settings.FONTSIZESTEP;
+                Settings.LINEHEIGHT = (int) (Settings.FONTSIZE * 1.2);
+            }
+            this.setMainView();
+            break;
+        case MENU_FONT_DOWN:
+            if (Settings.FONTSIZE > Settings.FONTSIZEMIN) {
+                Settings.FONTSIZE -= Settings.FONTSIZESTEP;
+                Settings.LINEHEIGHT = (int) (Settings.FONTSIZE * 1.2);
+            }
+            this.setMainView();
+            break;
+        default:
+            break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setMainView() {
+        setContentView(R.layout.main);
+        myGLView = (BaseGLSurfaceView) this.findViewById(R.id.myView1);
+        myGLView.createDefaultRenderer();
+        text1 = (TextView) this.findViewById(R.id.textView1);
+        // myGLView = new MainView(this);
+        // myGLView.createDefaultRenderer();
+        // setContentView(myGLView);
     }
 
 }
--- src/com/bookreader/config/Settings.java
+++ src/com/bookreader/config/Settings.java
@@ -6,8 +6,12 @@ public class Settings {
 
     public static String FILENAME = "damoyao.txt";
     public static String CODESET = "gb2312";
+
+    public static final int FONTSIZEMAX = 60;
+    public static final int FONTSIZEMIN = 12;
+    public static final int FONTSIZESTEP = 4;
     public static int FONTSIZE = 24;
-    public static int LINEHEIGHT = 28;
+    public static int LINEHEIGHT = (int) (FONTSIZE * 1.2);
     public static int BUFFERSIZE = 2048;
     public static int OFFSET = 0;
     public static int NEXTPAGEOFFSET = 100;
@@ -16,9 +20,9 @@ public class Settings {
     public static int colorR = 0;
     public static int colorG = 0;
     public static int colorB = 0;
-    public static int bkgColorA = 192;
+    public static int bkgColorA = 96;
     public static int bkgColorR = 250;
     public static int bkgColorG = 250;
-    public static int bkgColorB = 154;
+    public static int bkgColorB = 174;
     public static Typeface typeFace = Typeface.DEFAULT;
 }
